e357a45c4ccd5370b5fc0cc0446b4275d816bebc
CArnoud <c.arnoud@hotmail.com>
2014-03-19 15:29:04 -0300

    Color Space conversion completed + new Pyramid cpp function

--- src/ColorSpace.cpp
+++ src/ColorSpace.cpp
@@ -16,6 +16,46 @@ Mat ColorSpace::rgbConvert(Mat I)
 
 Mat rgb2luv(Mat I)
 {
+    Mat result;
+    const double y0 = ((6.0/29)*(6.0/29)*(6.0/29));
+    const double a = ((29.0/3)*(29.0/3)*(29.0/3));
+    const double un = 0.197833, vn = 0.468331;
+    double mr[3] = {0.430574, 0.222015, 0.020183};
+    double mg[3] = {0.341550, 0.706655, 0.129553};
+    double mb[3] = {0.178325, 0.071330, 0.939180};
+    double maxi = 1.0/270, minu = -88*maxi, minv = -134*maxi;
+    static double lTable[1064];
+    double x, y, z, l;
+    for (int i=0; i<1025; i++)
+    {
+        y = i/1024.0;
+        if (y>y0)
+            l = 116*pow(y,1.0/3.0)-16;
+        else
+            l = y * a;
+        lTable[i] = l * maxi;
+    }
+    for (int i=1024; i<1064; i++)
+        lTable[i] = lTable[i-1];
+    for (int i = 0; i < I.rows*I.cols*I.channels(); i = i+3)
+    {
+        uchar b = I.data[i*I.step];
+        uchar g = I.data[i*I.step+1];
+        uchar r = I.data[i*I.step+2];
+        x = mr[0]*r + mg[0]*g + mb[0]*b;
+        y = mr[1]*r + mg[1]*g + mb[1]*b;
+        z = mr[2]*r + mg[2]*g + mb[2]*b;
+        l = lTable[(int)(y*1024)];
+        result.data[i*I.step] = l; //l
+        z = 1/(x + 15*y + 3*z + 1e-35);
+        result.data[i*I.step+1] = l * (13*4*x*z - 13*un) - minu; //u
+        result.data[i*I.step+2] = l * (13*9*y*z - 13*vn) - minv; //v
+    }
+    return result;
+}
+
+Mat rgb2hsv(Mat I)
+{
     //image will be represented as h,s,v
     Mat result;
     uchar h, s, v, maxValue=0, minValue=0;
@@ -68,14 +108,6 @@ Mat rgb2luv(Mat I)
         }
     }
     return result;
-}
-
-Mat rgb2hsv(Mat I)
-{
-    for (int i = 0; i < I.rows*I.cols*I.channels(); i = i+3)
-    {
-        ;
-    }
 }
 
 Mat rgb2gray (Mat I)
--- src/Pyramid.cpp
+++ src/Pyramid.cpp
@@ -5,7 +5,7 @@ Pyramid computeMultiScaleChannelFeaturePyramid(Mat)
     ;
 }
 
-Pyramid computeSingleScaleChannelFeaturePyramid(Mat I, Channel pChns)
+Pyramid Pyramid::computeSingleScaleChannelFeaturePyramid(Mat I, Channel pChns)
 {
     //crop I so it becomes divisible by shrink
     int height = I.rows - (I.rows % pChns.shrink);
@@ -13,5 +13,45 @@ Pyramid computeSingleScaleChannelFeaturePyramid(Mat I, Channel pChns)
 
 
     //compute color channels
+    I = this->pChns.pColor.rgbConvert(I);
+}
 
+Mat TriangleFilterConvolution(Mat I, int r, int s, int nomex)
+{
+    Mat result = I;
+    if(!I.empty() && !(r==0 && s==1))
+    {
+        int m = min(I.rows, I.cols);
+        if(m>=4 && m>2*r+1)
+        {
+            if(nomex==0)
+            {
+                if(r>0 && r<=1 && s<=2)
+                    result = convConst("convTri1",I,12/r/(r+2)-2,s);
+                else
+                    result = convConst("convTri", I, r, s);
+            }
+            else
+            {
+                if (r <= 1)
+                {
+                    double p=12/r/(r+2)-2;
+                    f = [1 p 1]/(2+p);
+                    r=1;
+                }
+                else
+                {
+                    f=[1:r r+1 r:-1:1]/(r+1)^2;
+                }
+                result = padarray(I,[r r],"symmetric","both");
+                //result = convn(convn(result,f,"valid"),f',"valid");
+                if (s>1)
+                {
+                    t=floor(s/2)+1;
+                    result = result(t:s:end-s+t,t:s:end-s+t,:);
+                }
+            }
+        }
+    }
+    return result;
 }
