b4c7d1d63c82df12063f3f58d23b107cb1135f3f
CArnoud <c.arnoud@hotmail.com>
2014-03-20 22:02:53 -0300

    More Pyramid.cpp work

--- src/Pyramid.cpp
+++ src/Pyramid.cpp
@@ -33,21 +33,32 @@ Mat TriangleFilterConvolution(Mat I, int r, int s, int nomex)
             }
             else
             {
+                double f[3];
                 if (r <= 1)
                 {
                     double p=12/r/(r+2)-2;
-                    f = [1 p 1]/(2+p);
+                    f[0] = 1/(2+p);
+                    f[2] = f[0];
+                    f[3] = p / (2+p);
                     r=1;
                 }
                 else
                 {
-                    f=[1:r r+1 r:-1:1]/(r+1)^2;
+                    for (int i=1; i <= r+1; i++)
+                        f[i] = i/(pow(r+1,2));
+                    for (int i=2*r+1; i > r+1; i--)
+                        f[i] = i/(pow(r+1,2));
                 }
-                result = padarray(I,[r r],"symmetric","both");
+                //result = padarray(I,[r r],"symmetric","both");
+                copyMakeBorder(I, result, 0, 0, r, r, BORDER_REPLICATE);
                 //result = convn(convn(result,f,"valid"),f',"valid");
                 if (s>1)
                 {
-                    t=floor(s/2)+1;
+                    int t=floor(s/2)+1;
+                    Mat temp;
+                    /*for (int i=t; i < result.rows-s+t+1; i = i + s)
+                        for (int j=t; j < result.cols-s+t+1; j = j + s)
+                            temp*/
                     result = result(t:s:end-s+t,t:s:end-s+t,:);
                 }
             }
